303bb3553620234a4d529f4316a1448e1d3bc0a1
Qian <wubin28@outlook.com>
2022-10-08 09:24:30 +0800

    create a blank project using start.spring.io

---
+++ src/main/java/com/mycodekata/fizzbuzzforci/FizzbuzzforciApplication.java
@@ -0,0 +1,13 @@
+package com.mycodekata.fizzbuzzforci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzforciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzforciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycodekata/fizzbuzzforci/FizzbuzzforciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycodekata.fizzbuzzforci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzforciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
